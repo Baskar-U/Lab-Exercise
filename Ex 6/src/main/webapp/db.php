2cfa939b4851b87463b33386c1391fb95c4f58e7
Baskar-12345 <baskar.26csa@licet.ac.in>
2024-11-26 18:40:33 +0530

    Your Commit Message

--- Ex 6/src/main/webapp/db.php
+++ Ex 6/src/main/webapp/db.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "password";
-$dbname = "yourDB";
+$dbname = "ticket";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
